7dc0565bdffe51448d06dba5c532bf3d52a630bb
Cristian Lara <cristian.lara@upeu.edu.pe>
2022-11-21 22:27:09 -0500

    first commit

---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { IconsComponent } from '../../pages/icons/icons.component';
+import { MapsComponent } from '../../pages/maps/maps.component';
+import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
+import { TablesComponent } from '../../pages/tables/tables.component';
+import { RequisitosComponent } from '../../pages/requisitos/requisitos.component';
+import { OrganizacionesSocialesComponent } from '../../pages/organizaciones-sociales/organizaciones-sociales.component';
+
+
+
+export const AdminLayoutRoutes: Routes = [
+    { path: 'dashboard',      component: DashboardComponent },
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'tables',         component: TablesComponent },
+    { path: 'icons',          component: IconsComponent },
+    { path: 'maps',           component: MapsComponent },
+    { path: 'requisitos',           component: RequisitosComponent },
+    { path: 'organizaciones-sociales',           component: OrganizacionesSocialesComponent }
+];
